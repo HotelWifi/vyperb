f3d912969857ac923f02abd20502654c62c48bbc
Developer <developer@pop-os.localdomain>
2020-08-04 19:45:06 -0500

    Adding the client to the functions

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,25 +1,23 @@
-const economy = require('../../util/economy')
-const Discord = require("discord.js")
+const economy = require("../../util/economy");
+const Discord = require("discord.js");
 const botconfig = require("../../botsettings.json");
 
-module.exports.run = async (message, args) => {
+module.exports.run = async (client, message, args) => {
+  const target = message.mentions.users.first() || message.author;
+  const targetId = target.id;
 
-    const target = message.mentions.users.first() || message.author
-    const targetId = target.id
+  const guildId = message.guild.id;
+  const userId = target.id;
 
-    const guildId = message.guild.id
-    const userId = target.id
+  const coins = await economy.getCoins(guildId, userId);
 
-    const coins = await economy.getCoins(guildId, userId)
-
-    message.reply(`That user has ${coins} coins!`)
-  }
+  message.reply(`That user has ${coins} coins!`);
+};
 
 module.exports.config = {
-    name: "bal",
-    description: "Shows a user's balance",
-    usage: "bal",
-    accessableby: "v?bal",
-    aliases: ["balance", "coins"]
-}
-  
+  name: "bal",
+  description: "Shows a user's balance",
+  usage: "bal",
+  accessableby: "v?bal",
+  aliases: ["balance", "coins"],
+};
